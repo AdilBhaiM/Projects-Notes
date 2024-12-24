929a3273df28feecfcf9a1066aeb8c238464f0c2
Adil <mrkabsiejossw@gmail.com>
2024-12-24 13:38:51 +0500

    Node and babel is done(Code compilation is Done.)

--- Node/Babel/prd/index.js
+++ Node/Babel/prd/index.js
@@ -3,10 +3,11 @@
 var _express = _interopRequireDefault(require("express"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
 // import path from 'path';
+
 var app = (0, _express["default"])();
 var port = 3000;
 app.get('/', function (req, res) {
-  res.send('Hello Adil');
+  res.send('Hello 7667676776');
 });
 app.listen(port, function () {
   console.log("Example app is listening at localhost:".concat(port));
--- Node/Babel/src/index.js
+++ Node/Babel/src/index.js
@@ -1,9 +1,9 @@
-import express from 'express';
 // import path from 'path';
+import express from 'express';
 const app = express();
 const port = 3000;
 app.get('/', (req, res)=>{
-    res.send('Hello Hello')
+    res.send('Hello 7667676776')
 })
 
  app.listen(port, ()=>{
