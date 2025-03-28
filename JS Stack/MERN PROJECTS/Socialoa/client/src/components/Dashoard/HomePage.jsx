ea2a12510a11fa09f0bcdc5792c74d1265a8bba6
AdilBhaiM <mrkabsiejossw@gmail.com>
2025-03-28 14:49:13 +0500

    Changings

--- JS Stack/MERN PROJECTS/Socialoa/client/src/components/Dashoard/HomePage.jsx
+++ JS Stack/MERN PROJECTS/Socialoa/client/src/components/Dashoard/HomePage.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className='text-white'>HomePage</div>
+    <div className='flex flex-col w-full'>
+      <div className="flex text-white gap-2 w-full">
+        <div className="flex flex-col gap-2 min-w-[180] flex-1">
+          <p className='font-thin'>Total Posts</p>
+          <h1 className='text-2xl'>123</h1>
+        </div>
+        <div className="flex flex-col gap-2 min-w-[180] flex-1">
+          <p className='font-thin'>Scheduled Posts</p>
+          <h1 className='text-2xl'>123</h1>
+        </div>
+        <div className="flex flex-col gap-2 min-w-[180] flex-1">
+          <p className='font-thin'>Draft Posts</p>
+          <h1 className='text-2xl'>123</h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
