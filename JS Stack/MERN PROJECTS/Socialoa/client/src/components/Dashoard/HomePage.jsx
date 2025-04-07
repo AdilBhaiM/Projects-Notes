dc26ca8cf34ab572971412c1bdcdb2bc9e3c9abd
AdilBhaiM <mrkabsiejossw@gmail.com>
2025-04-07 22:48:01 +0500

    changes

--- JS Stack/MERN PROJECTS/Socialoa/client/src/components/Dashoard/HomePage.jsx
+++ JS Stack/MERN PROJECTS/Socialoa/client/src/components/Dashoard/HomePage.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const HomePage = () => {
+
+
+  
   return (
     <div className='flex flex-col w-full'>
       <div className="flex text-white gap-2 w-full">
@@ -15,12 +18,6 @@ const HomePage = () => {
         <div className="flex flex-col gap-2 min-w-[180] flex-1">
           <p className='font-thin'>Draft Posts</p>
           <h1 className='text-2xl'>123</h1>
-
-
-
-          
-
-          
         </div>
       </div>
     </div>
