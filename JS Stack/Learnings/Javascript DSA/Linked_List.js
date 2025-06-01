7357dbf089456134b9f2284d8e2ff2dbfc648175
AdilBhaiM <mrkabsiejossw@gmail.com>
2025-06-02 04:31:26 +0500

    Secure creds

---
+++ JS Stack/Learnings/Javascript DSA/Linked_List.js
@@ -0,0 +1,52 @@
+// Recommended ----  First practice how to use classes in real time then jump into the NodeList aka linkedList concept
+
+// class usersAttendance {
+//     userCount = 0;
+//     constructor(username) {
+//         this.username = username;
+//         usersAttendance.userCount++;
+//     }
+
+//     static printCount(username) {
+//         console.log(`Total Users Count is ${this.userCount} and Printer name is ${username}`)
+//     }
+
+//     printUsername() {
+//         console.log(`Hello my name is ${this.username}`)
+//     }
+// }
+
+// const Names = ["John", "Stacy", "Micheal"];
+
+// for (let name of Names) {
+//     const currName = new usersAttendance(name)
+//     currName.printUsername()
+// }
+// usersAttendance.printCount("Adil")
+
+
+
+
+// Linked List ========-----------------==========
+
+
+class Node {
+    constructor(data){
+        this.data = data,
+        this.next = null
+    }
+}
+
+class linkedList {
+    constructor(){
+        this.head = null
+        this.size = 0
+    }
+
+}
+
+
+new linkedList()
+const newNode = new Node(null);
+
+console.log(newNode);
