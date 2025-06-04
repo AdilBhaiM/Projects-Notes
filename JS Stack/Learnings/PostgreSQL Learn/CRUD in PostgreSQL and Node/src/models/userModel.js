454d3845662dc10962913e84d299b8341d3c8612
AdilBhaiM <mrkabsiejossw@gmail.com>
2025-06-05 03:26:15 +0500

    Changes formatting

--- JS Stack/Learnings/PostgreSQL Learn/CRUD in PostgreSQL and Node/src/models/userModel.js
+++ JS Stack/Learnings/PostgreSQL Learn/CRUD in PostgreSQL and Node/src/models/userModel.js
@@ -1,24 +1,29 @@
 import pool from '../config/db.js';
 
-
-
-
 const getAllUsersServices = async ()=>{
     const result = await pool.query("SELECT * FROM users");
     return result.rows;
 }
+
+
 const getUserByIdServices = async (id)=>{
     const result = await pool.query("SELECT * FROM users where id = $1", [id]);
     return result.rows[0];
 }
+
+
 const createUserServices = async (name, email)=>{
     const result = await pool.query("INSERT INTO users  (name, email) VALUES ($1, $2) RETURNING *", [name , email]);
     return result.rows[0];
 }
+
+
 const updateUserByIdServices = async (id, name , email)=>{
     const {id} = req.params
     const result = await pool.query('UPDATE users SET name=$1, email=$2 where id = $3 RETURNING *', [name, email])
 }
+
+
 const deleteUserServices = async (id)=>{
     const result = await pool.query("DELETE FROM users WHERE id = $1 RETURNING *", [id])
     return result.rows[0];
