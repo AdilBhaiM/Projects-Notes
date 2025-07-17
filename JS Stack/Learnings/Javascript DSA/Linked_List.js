90a347d5637af0b5387ef667ce460a8b21094d7b
AdilBhaiM <mrkabsiejossw@gmail.com>
2025-07-17 23:25:53 +0500

    Practice for DSA

--- JS Stack/Learnings/Javascript DSA/Linked_List.js
+++ JS Stack/Learnings/Javascript DSA/Linked_List.js
@@ -52,31 +52,65 @@
 // console.log(newNode);
 
 
+
+
+// Valid Paranthesis Problem ========-----------------==========
+
 /**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
-    let stack = [];
+//  * @param {string} s
+//  * @return {boolean}
+//  */
+// var isValid = function(s) {
+//     let stack = [];
 
-    for (let i=0; i<s.length; i++){
-        let char = s[i];
+//     for (let i=0; i<s.length; i++){
+//         let char = s[i];
         
-        if(char === '(' || char === '[' || char === '{'){
-            stack.push(char);
+//         if(char === '(' || char === '[' || char === '{'){
+//             stack.push(char);
 
-        }
-        else {
-            let last = stack.pop();
-            if(
-                (char === ')' && last !== '(') ||
-                (char === ']' && last !== '[') ||
-                (char === '}' && last !== '{')
-            ) return false;
+//         }
+//         else {
+//             let last = stack.pop();
+//             if(
+//                 (char === ')' && last !== '(') ||
+//                 (char === ']' && last !== '[') ||
+//                 (char === '}' && last !== '{')
+//             ) return false;
+//         }
+//     }
+
+//     return stack.length === 0;
+// };
+
+// console.log(isValid('{[]}'));
+
+
+
+
+// Longest Palindromic Substring Problem ========-----------------==========
+
+var longestPalindrome = function(s) {
+    let palindromFound = false;
+    let finalString = "";
+    if (s.length <= 1 || (s.length == 2 && s[0] !== s[1])) {
+        finalString = s[0];
+    }else{
+        for(let i=0; i <= s.length; i++){
+            finalString = s[i];
+            let partialValue = "";
+            for(let j=i+1; j <= s.length; j++){
+                partialValue = partialValue + s[j]
+                if(s[j] === finalString){
+                    palindromFound = true;
+                    break;
+                }
+            }
+            if(palindromFound === true) break;
         }
     }
-
-    return stack.length === 0;
+    return finalString;
 };
 
-console.log(isValid('{[]}'));
+
+console.log(longestPalindrome("aa"));
