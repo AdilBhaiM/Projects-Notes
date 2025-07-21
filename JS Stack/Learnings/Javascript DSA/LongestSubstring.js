a6370b5a81effb091bc4e650e567bcc05cc357ba
AdilBhaiM <mrkabsiejossw@gmail.com>
2025-07-22 02:52:14 +0500

    Longest Substring solution 2 By myself

--- JS Stack/Learnings/Javascript DSA/LongestSubstring.js
+++ JS Stack/Learnings/Javascript DSA/LongestSubstring.js
@@ -3,16 +3,23 @@
  * @param {string} s - The input string.
  * @return {number} - The length of the longest substring without repeating characters.
  */
+
 function lengthOfLongestSubstring(s) {
     let currObj = {};
-    // let currSubString = [];
+    let startingIndex = 0;
     let maxLen = 0;
-    for (let i = 0; i < s.length; i++){
+    for (let i = 0; i < s.length; i++) {
         const currChar = s[i];
-        if(currObj[i] !== undefined && maxLen > Object.keys(currObj).length){
-
+        if (currObj[currChar] !== undefined && currObj[currChar] >= startingIndex) {
+            startingIndex = currObj[currChar] + 1;
+        }
+        currObj[currChar] = i;
+        const currentWindowLength = i - startingIndex + 1;
+        if (currentWindowLength > maxLen) {
+            maxLen = currentWindowLength;
         }
     }
+    return maxLen;
 }
 
 console.log(lengthOfLongestSubstring("oshodfaoshf"));
