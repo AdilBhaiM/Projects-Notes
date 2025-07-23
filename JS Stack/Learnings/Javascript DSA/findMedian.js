3e55912bc624186f775fb81a0a41b2d5762794a9
AdilBhaiM <mrkabsiejossw@gmail.com>
2025-07-24 02:30:04 +0500

    Finding Median 2

--- JS Stack/Learnings/Javascript DSA/findMedian.js
+++ JS Stack/Learnings/Javascript DSA/findMedian.js
@@ -5,8 +5,8 @@
  */
 var findMedianSortedArrays = function(nums1, nums2) {
     let mergedArr = nums1.concat(nums2).sort()
-
-    console.log(mergedArr)
+    const sum = mergedArr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    return (sum / mergedArr.length)
 };
 
 findMedianSortedArrays([3,5,8,6], [1,9,2,4])
