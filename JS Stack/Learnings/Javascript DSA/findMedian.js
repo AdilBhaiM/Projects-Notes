75674799810d06eb5c8653e075e64c96ad9cbd66
AdilBhaiM <mrkabsiejossw@gmail.com>
2025-08-01 02:12:52 +0500

    Find median of an array

--- JS Stack/Learnings/Javascript DSA/findMedian.js
+++ JS Stack/Learnings/Javascript DSA/findMedian.js
@@ -4,9 +4,17 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-    let mergedArr = nums1.concat(nums2).sort()
-    const sum = mergedArr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-    return (sum / mergedArr.length)
+    let mergedArr = nums1.concat(nums2).sort((a, b) => a - b)
+    const len = mergedArr.length
+    if(len % 2 === 1){
+        // Odd number of elements, return the middle one
+        return mergedArr[Math.floor(len / 2)];
+    } else {
+        // Even number of elements, return the average of two middle elements
+        let mid1 = mergedArr[len / 2 - 1];
+        let mid2 = mergedArr[len / 2];
+        return (mid1 + mid2) / 2;
+    }
 };
 
 findMedianSortedArrays([3,5,8,6], [1,9,2,4])
