8a828824d749af5ff4484a84c26694d5e2768e5c
Adil <mrkabsiejossw@gmail.com>
2025-10-29 04:16:44 +0500

    Array DSA

--- JS Stack/Learnings/Javascript DSA/arrayTwoSum.js
+++ JS Stack/Learnings/Javascript DSA/arrayTwoSum.js
@@ -4,5 +4,19 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    
+  const seen = new Map(); // stores num → index
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    // If we've seen the complement, we found a pair
+    if (seen.has(complement)) {
+      return [seen.get(complement), i];
+    }
+
+    // Otherwise, store current number with its index
+    seen.set(nums[i], i);
+  }
+
+  return []; // No pair found
 };
