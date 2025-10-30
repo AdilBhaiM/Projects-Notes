3ee745ef01481e8cc224142a0dbf6fef009b050e
Adil <mrkabsiejossw@gmail.com>
2025-10-30 23:03:29 +0500

    DSA

--- JS Stack/Learnings/Javascript DSA/arrayTwoSum.js
+++ JS Stack/Learnings/Javascript DSA/arrayTwoSum.js
@@ -4,10 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    
   const seen = new Map();
 
   for (let i = 0; i < nums.length; i++) {
-    
+
     const complement = target - nums[i];
 
     if (seen.has(complement)) {
