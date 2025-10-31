e241f04b34ecdaa5f648db4c2d10ac1b1a90a51d
Adil <mrkabsiejossw@gmail.com>
2025-10-31 17:57:14 +0500

    DSA

--- JS Stack/Learnings/Javascript DSA/arrayLongestCommonPrefix.js
+++ JS Stack/Learnings/Javascript DSA/arrayLongestCommonPrefix.js
@@ -3,6 +3,17 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    
-    
+    if (!strs.length) return "";
+
+    let prefix = strs[0];
+
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            // shorten the prefix until it matches the start
+            prefix = prefix.slice(0, -1);
+            if (!prefix) return "";
+        }
+    }
+
+    return prefix;
 };
