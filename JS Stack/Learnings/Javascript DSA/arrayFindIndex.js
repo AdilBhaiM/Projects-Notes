c3e0d731fea9bd842ad439f7457d3c92c58e9ba7
Adil <mrkabsiejossw@gmail.com>
2025-11-01 02:12:44 +0500

    DSA

--- JS Stack/Learnings/Javascript DSA/arrayFindIndex.js
+++ JS Stack/Learnings/Javascript DSA/arrayFindIndex.js
@@ -4,16 +4,17 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
+
     for(let i = 0; i < nums.length; i++){
+
         if(nums[i] === target){
             return i
         }else if(nums[i] > target){
             return i
         }else if(i === (nums.length-1)){
-            // console.log(nums.length-1)
-            // console.log(i)
             return i+1
         }
+        
     }
 };
 
