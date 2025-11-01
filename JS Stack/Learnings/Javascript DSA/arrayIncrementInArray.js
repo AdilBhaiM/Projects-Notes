896e0d681244185f660dd7893622a10694e4d4d2
Adil <mrkabsiejossw@gmail.com>
2025-11-02 04:57:30 +0500

    DSA

--- JS Stack/Learnings/Javascript DSA/arrayIncrementInArray.js
+++ JS Stack/Learnings/Javascript DSA/arrayIncrementInArray.js
@@ -3,20 +3,27 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
+
     let i = -1
+
     while(i >= (digits.length * -1)){
+
         if(digits[digits.length + i] < 9){
+
             digits[digits.length + i] += 1;
             return digits;
+
         }else if(digits[digits.length + i] === 9 && digits.length + i === 0){
+
             digits[digits.length + i] = 0;
             digits.unshift(1)
-
-            
             return digits;
+
         }else{
+
             digits[digits.length + i] = 0;
             i += -1;
+
         }
     }
 };
