853a88aa848b35d6c4a94a928454376e4f4ff35e
Adil <mrkabsiejossw@gmail.com>
2025-11-01 22:37:48 +0500

    DSA

--- JS Stack/Learnings/Javascript DSA/arrayIncrementInArray.js
+++ JS Stack/Learnings/Javascript DSA/arrayIncrementInArray.js
@@ -2,18 +2,16 @@
  * @param {number[]} digits
  * @return {number[]}
  */
-
 var plusOne = function(digits) {
-
     let i = -1
     while(i >= (digits.length * -1)){
-
-        if(digits[i] < 9){
-            digit[i] += 1;
-            return digit;
+        if(digits[digits.length + i] < 9){
+            digits[digits.length + i] += 1;
+            return digits;
         }
-
-        digits[i] = 0;
+        digits[digits.length + i] = 0;
         i += -1;
     }
 };
+let result = plusOne([1, 2, 3, 4])
+console.log(result)
