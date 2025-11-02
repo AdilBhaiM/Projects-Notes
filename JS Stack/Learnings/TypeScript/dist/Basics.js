55fcc8e19857c84d70e2f461a670aa0de41c1b27
Adil <mrkabsiejossw@gmail.com>
2025-11-02 21:35:57 +0500

    DSA

---
+++ JS Stack/Learnings/TypeScript/dist/Basics.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Hello World");
+let age = 3;
+if (age < 50) {
+    console.log(age);
+    age += 10;
+}
+//# sourceMappingURL=Basics.js.map
---
+++ JS Stack/Learnings/TypeScript/src/Basics.ts
@@ -0,0 +1,8 @@
+console.log("Hello World")
+
+let age : number = 3;
+
+if(age < 50){
+    console.log(age)
+    age += 10;
+}
