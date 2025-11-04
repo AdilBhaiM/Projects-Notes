6adc6cc9131131768caebe6f9db41ef754d68df1
Adil <mrkabsiejossw@gmail.com>
2025-11-04 22:05:40 +0500

    DSA

--- JS Stack/Learnings/Javascript DSA/arrayPascal.js
+++ JS Stack/Learnings/Javascript DSA/arrayPascal.js
@@ -13,3 +13,16 @@
 
 //     return parArr[rowIndex];
 // }
+
+
+
+// function getRow(rowIndex: number): number[] {
+//     let row = [1];
+//     for (let i = 1; i <= rowIndex; i++) {
+//         for (let j = i - 1; j > 0; j--) {
+//             row[j] = row[j] + row[j - 1];
+//         }
+//         row.push(1);
+//     }
+//     return row;
+// }
