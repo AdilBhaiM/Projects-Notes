52473078b6ac7e14b5e46cfe70ffb11bb0a53005
Adil <mrkabsiejossw@gmail.com>
2025-11-05 03:46:40 +0500

    DSA

--- JS Stack/Learnings/OOP/objects.js
+++ JS Stack/Learnings/OOP/objects.js
@@ -1,9 +1,10 @@
 let arr = [1, 2, 3, 4, 5, 6, 7, 8]
 
 const caat_do_isko = {
-    caat_dall(){
-        console.log("kaat dya isko menee hahahha.. ", arr.pop())
+    'bakra' : 0,
+    caat_dall(kaat_isko){
+        console.log("kaat dya isko menee hahahha.. ", arr.splice(kaat_isko, 1)[0])
     }
 }
-caat_do_isko.caat_dall()
+caat_do_isko.caat_dall(2)
 console.log(arr)
