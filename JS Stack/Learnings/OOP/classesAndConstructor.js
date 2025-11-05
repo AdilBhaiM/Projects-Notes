09e6c2183c97497edeb4357edc16c55742429d2e
Adil <mrkabsiejossw@gmail.com>
2025-11-06 02:17:36 +0500

    DSA

---
+++ JS Stack/Learnings/OOP/classesAndConstructor.js
@@ -0,0 +1,25 @@
+class Hello {
+    constructor(name){
+        this.name = name
+        console.log("Hello, I am Hello constructor")
+    }
+    heySayIt(to){
+        console.log("Hey,", to)
+    }
+}
+
+class Greetings extends Hello{
+    constructor(getout){
+        super("No") // -------> Without this javascript will throw an error that you are not creating the instance of the Child class. so please add this keyword to run its constructor.
+        this.getOut = getout
+        console.log("Hello, I am Greetings constructor")
+    }
+    sayIt(){
+        console.log("Assalam u Alaikum,", this.name)
+    }
+}
+
+const hi = new Hello("Harry")
+const Salam = new Greetings("Sarah")
+hi.heySayIt("John")
+Salam.sayIt()
