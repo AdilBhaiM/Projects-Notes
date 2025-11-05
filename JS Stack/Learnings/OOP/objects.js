634312668e1e9f5ca91d58627533cf8227a77298
Adil <mrkabsiejossw@gmail.com>
2025-11-05 14:42:27 +0500

    DSA

--- JS Stack/Learnings/OOP/objects.js
+++ JS Stack/Learnings/OOP/objects.js
@@ -1,24 +1,25 @@
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8]
+let arr = [1, 2, 3, 4, 5, 6, 7, 8]
 
 
 
-// const kaat_do_isko = {
-//     'bakra' : 0,
-//     kaat_isko(kaat_de_isko){
-//         console.log("kaat dya isko menee hahahha.. ", arr.splice(kaat_de_isko, 1)[0])
-//     }
-// }
-// const mat_kaato_isko = {
-//     'bakra' : 0,
-//     mat_katna(mat_kat_isko){
-//         console.log("kaat dya isko menee hahahha.. ", arr.splice(mat_kat_isko, 1)[0])
-//     }
-// }
+const kaat_do_isko = {
+    'bakra' : 0,
+    kaat_isko(kaat_de_isko){
+        console.log("kaat dya isko menee hahahha.. ", arr.splice(kaat_de_isko, 1)[0])
+    }
+}
+const mat_kaato_isko = {
+    'bakra' : 0,
+    mat_katna(mat_kat_isko){
+        console.log("kaat dya isko menee hahahha.. ", arr.splice(mat_kat_isko, 1)[0])
+    }
+}
 
-// mat_kaato_isko.__proto__ = kaat_do_isko
-// // kaat_do_isko.__proto__ = mat_kaato_isko
+mat_kaato_isko.__proto__ = kaat_do_isko
 
-// kaat_do_isko.kaat_isko(2)
-// mat_kaato_isko.kaat_isko(6)
+// kaat_do_isko.__proto__ = mat_kaato_isko
 
-// console.log(arr)
+kaat_do_isko.kaat_isko(2)
+mat_kaato_isko.kaat_isko(6)
+
+console.log(arr)
