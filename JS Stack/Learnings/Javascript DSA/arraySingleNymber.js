8bd14ce5904ffa51a4e7bb8414bee55b4e33332e
Adil <mrkabsiejossw@gmail.com>
2025-11-06 04:19:42 +0500

    DSA

---
+++ JS Stack/Learnings/Javascript DSA/arraySingleNymber.js
@@ -0,0 +1,7 @@
+function singleNumber(nums) {
+  let result = 0;
+  for (let num of nums) {
+    result ^= num; // XOR cancels out duplicate numbers
+  }
+  return result;
+}
--- JS Stack/Learnings/OOP/classesAndConstructor.js
+++ JS Stack/Learnings/OOP/classesAndConstructor.js
@@ -1,28 +1,36 @@
-class Hello {
-    constructor(name){
-        this.name = name
-        console.log("Hello, I am Hello constructor")
-    }
-    heySayIt(to){
-        console.log("Hey,", to)
-    }
+// class Hello {
+//     constructor(name){
+//         this.name = name
+//         console.log("Hello, I am Hello constructor")
+//     }
+//     heySayIt(to){
+//         console.log("Hey,", to)
+//     }
+// }
+
+// class Greetings extends Hello{
+
+//     constructor(getout){
+//         super("No") // -------> Without this javascript will throw an error that you are not creating the instance of the Child class. so please add this keyword to run its constructor.
+//         this.getOut = getout
+//         console.log("Hello, I am Greetings constructor")
+//     }
+//     sayIt(){
+//         console.log("Assalam u Alaikum,", this.name)
+//     }
+// }
+
+// const hi = new Hello("Harry")
+
+
+// const Salam = new Greetings("Sarah")
+// hi.heySayIt("John")
+// Salam.sayIt()
+
+
+if(true === false){
+        console.log("Assalam u Alaikum, IFFF")
+}else if(true !== false){
+    console.log("Assalam u Alaikum, ELSEEE")
+
 }
-
-class Greetings extends Hello{
-
-    constructor(getout){
-        super("No") // -------> Without this javascript will throw an error that you are not creating the instance of the Child class. so please add this keyword to run its constructor.
-        this.getOut = getout
-        console.log("Hello, I am Greetings constructor")
-    }
-    sayIt(){
-        console.log("Assalam u Alaikum,", this.name)
-    }
-}
-
-const hi = new Hello("Harry")
-
-
-const Salam = new Greetings("Sarah")
-hi.heySayIt("John")
-Salam.sayIt()
