232f22bf6eb754d8407c771a140ad64f13c248f6
Adil <mrkabsiejossw@gmail.com>
2025-11-07 03:23:40 +0500

    DSA

--- JS Stack/Learnings/Javascript DSA/arraySingleNymber.js
+++ JS Stack/Learnings/Javascript DSA/arraySingleNymber.js
@@ -8,4 +8,22 @@ function singleNumber(nums) {
 }
 
 let res = singleNumber([1, 2, 6, 1, 6])
-console.log("RESULT :" , res)
+console.log("RESULT 1 :" , res)
+
+
+// -----------> Single Number 2 -------------
+
+
+function singleNumber2(nums) {
+  let ones = 0, twos = 0;
+
+  for (let num of nums) {
+    ones = (ones ^ num) & ~twos;
+    twos = (twos ^ num) & ~ones;
+  }
+
+  return ones;
+}
+
+let res2 = singleNumber2([1, 2, 6, 1, 6, 6, 1])
+console.log("RESULT 2 :" , res2)
