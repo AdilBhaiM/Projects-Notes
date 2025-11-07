382127c27f2b810bb612fe3b23f85f6c209cc617
Adil <mrkabsiejossw@gmail.com>
2025-11-07 22:48:12 +0500

    DSA

--- JS Stack/Learnings/Javascript DSA/arrayRemoveElements.js
+++ JS Stack/Learnings/Javascript DSA/arrayRemoveElements.js
@@ -9,11 +9,13 @@ var removeElement = function(nums, val) {
 
     let k = 0;
     for(i = 0; i < nums.length; i++){
-        
+
         if(nums[i] !== val){
             nums[k] = nums[i]
             k = k + 1;
         }
+
+        
     }
     return k 
 };
