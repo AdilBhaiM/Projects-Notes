492f173472469d6f9bd0ceec0b3e25add94d3c47
Adil <mrkabsiejossw@gmail.com>
2025-11-10 01:17:18 +0500

    DSA

--- JS Stack/Learnings/Next Js Learning/First Next JS App/my-next-app/src/dbConfig/dbConfig.ts
+++ JS Stack/Learnings/Next Js Learning/First Next JS App/my-next-app/src/dbConfig/dbConfig.ts
@@ -1,9 +1,19 @@
 import mongoose from 'mongoose'
 
 
-export async function connect() => {
+export async function connect() {
     try {
+        mongoose.connect(process.env.MONGO_URI!)
+        const connection = mongoose.connection;
+        connection.on("connected" , () => {
+            console.log("MongoDB connected Successfully");
+        })
         
+        connection.on("error" , () => {
+            console.log("MongoDB connection error. Please make sure mongoDB is running.");
+            process.exit();
+        })
+
     } catch (error) {
         console.log("Something went wrong!");
         console.log(error);
