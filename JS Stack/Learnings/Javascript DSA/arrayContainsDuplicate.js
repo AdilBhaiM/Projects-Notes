d21b0026f54d86c410ea12631cdb44b50d2b5dd0
Adil <mrkabsiejossw@gmail.com>
2025-11-14 23:57:19 +0500

    DSA

--- JS Stack/Learnings/Javascript DSA/arrayContainsDuplicate.js
+++ JS Stack/Learnings/Javascript DSA/arrayContainsDuplicate.js
@@ -2,7 +2,10 @@
  * @param {number[]} nums
  * @param {number} k
  * @return {boolean}
+ * 
  */
+
+
 var containsNearbyDuplicate = function(nums, k) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
@@ -14,3 +17,5 @@ var containsNearbyDuplicate = function(nums, k) {
     return false;
 
 };
+
+
