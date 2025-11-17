5c88e3fcec428e6eb7eec3203ea9a0cbcc11e457
Adil <mrkabsiejossw@gmail.com>
2025-11-17 18:54:31 +0500

    Contain Duplicates

--- JS Stack/Learnings/Javascript DSA/arrayMissingNumber.js
+++ JS Stack/Learnings/Javascript DSA/arrayMissingNumber.js
@@ -3,13 +3,16 @@ var missingNumber = function(nums) {
     const n = nums.length;
     
     // XOR all numbers from 0..n
+
     for (let i = 0; i <= n; i++) {
         xor ^= i;
     }
     
     // XOR all elements of nums
+    
     for (let num of nums) {
         xor ^= num;
+
     }
     
     return xor;
