b27eb7f6282ca38a90a81d84043457dcbc39d0dc
Adil <mrkabsiejossw@gmail.com>
2025-11-18 23:59:47 +0500

    Move Zeros

--- JS Stack/Learnings/Javascript DSA/arrayMoveZeros.py
+++ JS Stack/Learnings/Javascript DSA/arrayMoveZeros.py
@@ -3,8 +3,9 @@ class Solution:
     def moveZeroes(self, nums) -> None:
 
         num = 0
+        
         for i in range(len(nums)):
-            
+
             if nums[i] != 0:
 
                 nums[num], nums[i] = nums[i], nums[num]
