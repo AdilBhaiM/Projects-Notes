0425ea131602c2c8e4bd427cf2c2b39ab2226651
Adil <mrkabsiejossw@gmail.com>
2025-11-18 23:59:22 +0500

    Move Zeros

--- JS Stack/Learnings/Javascript DSA/arrayMoveZeros.py
+++ JS Stack/Learnings/Javascript DSA/arrayMoveZeros.py
@@ -5,6 +5,7 @@ class Solution:
         num = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                
+
                 nums[num], nums[i] = nums[i], nums[num]
+                
                 num += 1
