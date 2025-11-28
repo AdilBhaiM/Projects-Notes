ca381c4c787533faf4c5ecf040baaea808688ae4
Adil <mrkabsiejossw@gmail.com>
2025-11-28 15:59:57 +0500

    R2Int

--- Learn C++/main.cpp
+++ Learn C++/main.cpp
@@ -4,9 +4,10 @@ using namespace std; // It lets us to not to write std before every cout such as
 int main(){
 
     int a, b, c;
+    
     a = 1;
     short sa = 3;
-    
+
     cout<<sa<<endl<<a<<endl;
 
     // cout<< "Hello World\n";
