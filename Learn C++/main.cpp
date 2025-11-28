c9ba812d66a19d7b0570b3d769d5848d9e69e131
Adil <mrkabsiejossw@gmail.com>
2025-11-28 16:00:05 +0500

    R2Int

--- Learn C++/main.cpp
+++ Learn C++/main.cpp
@@ -1,10 +1,11 @@
 # include <iostream>
+
 using namespace std; // It lets us to not to write std before every cout such as cout<< "Hello World\n";
 
 int main(){
 
     int a, b, c;
-    
+
     a = 1;
     short sa = 3;
 
