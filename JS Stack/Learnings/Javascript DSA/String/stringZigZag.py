082eb5b8e37dba7cb448fd3a37f930a5fbaa6f18
Adil <mrkabsiejossw@gmail.com>
2025-12-01 14:01:22 +0500

    R2Int

--- JS Stack/Learnings/Javascript DSA/String/stringZigZag.py
+++ JS Stack/Learnings/Javascript DSA/String/stringZigZag.py
@@ -1,27 +1,49 @@
+# class Solution:
+#     def convert(self, s: str, numRows: int) -> str:
+#         n = len(s)
+#         if n < numRows or n == 1 or numRows == 1:
+#             return s
+#         currRow = 0
+#         direction = +1
+#         myRows = ['' for _ in range(numRows)]
+#         for i in range(n):
+#             myRows[currRow] += s[i]
+#             if currRow == numRows-1 and direction == +1:
+#                 direction = -1
+                
+#             elif currRow == 0 and direction == -1:
+#                 direction = +1
+#             if direction == +1:
+#                 currRow += 1
+#             else:
+#                 currRow -= 1
+#             print(myRows)
+#             print(currRow)
+#         result = ''.join(myRows)
+#         return result
+
+
+
+# --------------> Optimized Solution
+
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
         n = len(s)
-        if n < numRows or n == 1 or numRows == 1:
-            
+        if numRows == 1 or n <= numRows:
             return s
         currRow = 0
         direction = +1
-        myRows = ['' for _ in range(numRows)]
+        myRows = [[] for _ in range(numRows)]
         for i in range(n):
-            myRows[currRow] += s[i]
-            if currRow == numRows-1 and direction == +1:
-                direction = -1
-                
-            elif currRow == 0 and direction == -1:
+            myRows[currRow].append(s[i])
+            if currRow == 0:
                 direction = +1
-            if direction == +1:
-                currRow += 1
-            else:
-                currRow -= 1
-            print(myRows)
-            print(currRow)
-        result = ''.join(myRows)
+            elif currRow == numRows - 1:
+                direction = -1
+            currRow += direction
+        result = ''.join([''.join(r) for r in myRows])
         return result
+
 a = Solution()
 b = a.convert("PAYPALISHIRING", 3)
 print(b)
