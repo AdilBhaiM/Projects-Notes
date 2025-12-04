59f5461d89cd2f9dbdf2308c419bb1ef3bfef596
Adil <mrkabsiejossw@gmail.com>
2025-12-04 19:12:30 +0500

    Hellp

--- JS Stack/Learnings/Javascript DSA/String/stringFindIndexOfFirstOccurence.py
+++ JS Stack/Learnings/Javascript DSA/String/stringFindIndexOfFirstOccurence.py
@@ -3,8 +3,6 @@ class Solution:
         n = len(needle)
         m = len(haystack)
         for i in range(m - n + 1):
-            
             if haystack[i:i+n] == needle:
                 return i
-
         return -1
