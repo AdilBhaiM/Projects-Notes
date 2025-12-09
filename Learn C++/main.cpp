5526e4000a32d6a7e940b5a6311f5886c6cc6561
Adil <mrkabsiejossw@gmail.com>
2025-12-09 22:16:10 +0500

    Hellp

--- Learn C++/main.cpp
+++ Learn C++/main.cpp
@@ -3,14 +3,10 @@
 using namespace std; // It lets us to not to write std before every cout such as cout<< "Hello World\n";
 
 int main(){
-
     int a, b, c;
-
     a = 1;
     short sa = 3;
-
     cout<<sa<<endl<<a<<endl;
-
     // cout<< "Hello World\n";
     // return 0;
 }
