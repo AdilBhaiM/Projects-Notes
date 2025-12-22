d1a9789d58e936e629aee88e9afd6b0e93efbcb9
Adil <mrkabsiejossw@gmail.com>
2025-12-22 14:56:07 +0500

    Water Problem

---
+++ JS Stack/Learnings/Javascript DSA/TwoPointers/ContainerOfWater.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        maxValue = 0
+        left = 0
+        right = len(height)
+        for i in range(len(height)):
+            if(left == right):
+                return maxValue
+            value = min(height[left], height[right]) * left-right
+            maxValue = max(maxValue, value)
+            if left < right:
+                left = i
+            else:
+                right = i - right
+
